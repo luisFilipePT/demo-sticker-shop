2ec1d15ee52b25f7e86736ef17da80396d86d695
Luis Oliveira (xGeeks) <l.oliveira@xgeeks.io>
2020-03-04 11:11:46 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>The future sticker shop.</p>
+    <p>Now let's build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
